8e3017c6716d113ded555e9d1f8c3abbfee613bc
oliverdantzer <104586131+oliverdantzer@users.noreply.github.com>
2023-10-02 18:25:00 -0400

    edit helper function

--- Lab 2/process_sync_sc1.py
+++ Lab 2/process_sync_sc1.py
@@ -8,10 +8,10 @@ class Request:
     # TODO: Add other necessary methods if required
 
 def start_scheduling(requests, time_quantum,method):
-    if method == 'robin':
-        round_robin(requests, time_quantum)
-    else:
-        second(requests, time_quantum)
+    algorithms = [round_robin, first_come_first_serve]
+    for algorithm in algorithms:
+        algorithm(requests, time_quantum)
+
 
 def round_robin(requests, time_quantum):
     queue = []
@@ -46,10 +46,6 @@ def round_robin(requests, time_quantum):
     #print(f"Average Turnaround Time: {total_turnaround_time/len(requests)}")
 
 
-def second(requests, time_quantum):
-    pass
-    
-    
 
 def first_come_first_serve(requests):
     # This function will handle the first come first serve algorithm
@@ -64,6 +60,7 @@ def first_come_first_serve(requests):
         turnaround_time = waiting_time + burst_time
         waiting_time_sum += waiting_time
         turnaround_time_sum += turnaround_time
+        print(f"Request ID: {request.id}, Waiting Time: {waiting_time}, Turnaround Time: {turnaround_time}, Arrival Time: {arrival_time}")
     #avg waiting time, avg turnaround time
     print(f"First come first serve:\nAverage waiting time: {waiting_time_sum / len(requests)}\nAverage turnaround time:{turnaround_time_sum / len(requests)}")
 
