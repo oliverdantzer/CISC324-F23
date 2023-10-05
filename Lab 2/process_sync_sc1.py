3093d027b5376eb86fbcb429d01d5559ab932399
Qays Ebrahim <qays.ebrahim@gmail.com>
2023-10-05 11:05:52 -0400

    Update process_sync_sc1.py

--- Lab 2/process_sync_sc1.py
+++ Lab 2/process_sync_sc1.py
@@ -7,11 +7,9 @@ class Request:
 
     # TODO: Add other necessary methods if required
 
-def start_scheduling(requests, time_quantum,arrivals,method):
+def start_scheduling(requests, time_quantum,arrivals):
     print("Round robin:")
     round_robin(requests, time_quantum,arrivals)
-    print("First come first serve:")
-    first_come_first_serve(requests, arrivals)
 
 
 def round_robin(requests, time_quantum, arrivals):
@@ -20,32 +18,46 @@ def round_robin(requests, time_quantum, arrivals):
     elapsed_time = 0
     total_turnaround_time = 0
     total_wait_time = 0
+    
+    #add requests to queue
     for req in requests:
         queue.append(req)
+    #sort queue by arrival time
     queue.sort(key=lambda x: arrivals[x.id])
     
     while (len(queue)>0):
+
+        #check if request has arrived
         if (elapsed_time >= arrivals[queue[0].id]):
+            #if request has arrived, pop it off the queue
             req = queue.pop(0)
+            #if request has remaining time greater than time quantum, process it and add it back to the queue
             if(req.remaining_time > time_quantum):
                 req.remaining_time -= time_quantum
+                #increment elapsed time by time quantum
                 elapsed_time+= time_quantum
             
                 queue.append(req)
             else:
+                #if request has remaining time less than time quantum, process it and calculate turnaround and waiting time
                 elapsed_time+=req.remaining_time
                 req.remaining_time = 0
                 turnaround[req.id] = elapsed_time - arrivals[req.id]
                 req.wait_time = turnaround[req.id] - req.processing_time
         else:
-            elapsed_time+=1
+            #if no requests have arrived, increment time to the first arrival
+            elapsed_time = arrivals[queue[0].id]
           
     for req in requests:
+        #print turnaround and waiting time for each request
         print(f"Request ID: {req.id}, Arrival Time: {arrivals[req.id]} Waiting Time: {req.wait_time}, Turnaround Time: {turnaround[req.id]}")
+        #calculate total turnaround and waiting time
         total_wait_time += req.wait_time
         total_turnaround_time += turnaround[req.id]
+        #reset request values
         req.wait_time = 0
         req.remaining_time = req.processing_time
+    #print average turnaround and waiting time
     print(f"Average Waiting Time: {total_wait_time/len(requests)}")
     print(f"Average Turnaround Time: {total_turnaround_time/len(requests)}")
 
@@ -58,24 +70,31 @@ def first_come_first_serve(requests, arrivals):
     elapsed_time = 0
     
     queue = []
+    
+    #add requests to queue
     for req in requests:
         queue.append(req)
-        queue.sort(key=lambda x: arrivals[x.id])
+    #sort queue by arrival time
+    queue.sort(key=lambda x: arrivals[x.id])
 
     while(len(queue)>0):
+        #check if request has arrived
         if (elapsed_time < arrivals[queue[0].id]):
             elapsed_time = arrivals[queue[0].id]
         else:
+            #if request has arrived, pop it off the queue, process it and calculate waiting and turnaround time 
             request = queue.pop(0)
             waiting_time = elapsed_time - arrivals[request.id]
             elapsed_time += request.processing_time
             turnaround_time = waiting_time + request.processing_time
 
+            #add waiting and turnaround time to total
             waiting_time_sum += waiting_time
             turnaround_time_sum += turnaround_time
+        
             print(f"Request ID: {request.id}, Waiting Time: {waiting_time}, Turnaround Time: {turnaround_time}, Arrival Time: {arrivals[request.id]}")
 
-    #avg waiting time, avg turnaround time
+    # print avg waiting time, avg turnaround time
     print(f"Average waiting time: {waiting_time_sum / len(requests)}\nAverage turnaround time:{turnaround_time_sum / len(requests)}")
 
     
@@ -98,33 +117,16 @@ def main():
     #generate random arrival times
     for i in range(len(requests)):
         arrivals[i] = random.randint(0,10)
-    
 
-    # print('\n**************Test case# 1**************\n')
-    # # Displaying generated requests
-    # for req in requests_tc1:
-    #     print(f"Request ID: {req.id}, Processing Time: {req.processing_time}")
-
-    # time_quantum = 3  # You can adjust this value based on requirements
-    # start_scheduling(requests_tc1, time_quantum,arrivals,"robin")
-
-
-    # print('\n**************Test case# 2**************\n')
-    # # Displaying generated requests
-    # for req in requests_tc2:
-    #     print(f"Request ID: {req.id}, Processing Time: {req.processing_time}")
-
-    # time_quantum = 3  # You can adjust this value based on requirements
-    # start_scheduling(requests_tc2, time_quantum,arrivals,"robin")
-
+    for req in requests:
+        print(f"Request ID: {req.id}, Processing Time: {req.processing_time}")
+    #run the tests for multiple time quantum values
     for time_quantum in ([1,3,7]):
-        #print value of time quantum
-        print(f"Time quantum: {time_quantum}")
-        # Displaying generated requests
-        for req in requests:
-            print(f"Request ID: {req.id}, Processing Time: {req.processing_time}")
 
-        start_scheduling(requests, time_quantum,arrivals,"robin")
+        print(f"Round Robin Time quantum: {time_quantum}")
+        start_scheduling(requests, time_quantum,arrivals)
+    print("First come first serve:")
+    first_come_first_serve(requests, arrivals)
 
     # TODO: Calculate and display the average waiting time and average turnaround time
 
